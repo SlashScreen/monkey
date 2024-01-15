3484d3e36302c1e26f6802a5054e63bf899ed5bf
Slashscreen <SlashScreen@users.noreply.github.com>
2024-01-14 21:49:24 -0800

    init. chapter 1 done

---
+++ lexer/lexer.go
@@ -0,0 +1,155 @@
+package lexer
+
+import (
+	"unicode/utf8"
+
+	"monkey/token"
+)
+
+var singleCharMatch = map[rune]token.TokenType{
+	'=': token.ASSIGN,
+	';': token.SEMICOLON,
+	'(': token.LPAREN,
+	')': token.RPAREN,
+	',': token.COMMA,
+	'+': token.PLUS,
+	'-': token.MINUS,
+	'*': token.ASTERISK,
+	'/': token.SLASH,
+	'{': token.LBRACE,
+	'}': token.RBRACE,
+	'^': token.CARET,
+	'|': token.PIPE,
+	'&': token.AMPERSAND,
+	'%': token.PERCENT,
+	'<': token.LANG,
+	'>': token.RANG,
+}
+
+var doubleCharMatch = map[string]token.TokenType{
+	token.EQ:      token.EQ,
+	token.NEQ:     token.NEQ,
+	token.GEQ:     token.GEQ,
+	token.LEQ:     token.LEQ,
+	token.XOR_EQ:  token.XOR_EQ,
+	token.OR_EQ:   token.OR_EQ,
+	token.AND_EQ:  token.AND_EQ,
+	token.MIN_EQ:  token.MIN_EQ,
+	token.MUL_EQ:  token.MUL_EQ,
+	token.DIV_EQ:  token.DIV_EQ,
+	token.PERC_EQ: token.PERC_EQ,
+	token.AND:     token.AND,
+	token.OR:      token.OR,
+	token.SHOVL:   token.SHOVL,
+	token.SHOVR:   token.SHOVR,
+}
+
+var keywordMatch = map[string]token.TokenType{
+	"fn":     token.FUNCTION,
+	"let":    token.LET,
+	"if":     token.IF,
+	"else":   token.ELSE,
+	"return": token.RETURN,
+	"true":   token.TRUE,
+	"false":  token.FALSE,
+	"!":      token.BANG, // putting bang here for convenience
+}
+
+type Lexer struct {
+	input        string
+	position     int
+	readPosition int
+	ch           rune
+}
+
+func New(input string) *Lexer {
+	l := &Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	width := 1
+
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch, width = utf8.DecodeRuneInString(l.input[l.readPosition:])
+	}
+
+	l.position = l.readPosition
+	l.readPosition += width
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var tok token.Token
+
+	l.eatWhitespace()
+
+	if l.ch == 0 {
+		tok = token.New(token.EOF, "")
+	} else if val, ok := doubleCharMatch[string(l.ch)+string(l.peekChar())]; ok {
+		tok = token.New(val, string(l.ch)+string(l.peekChar()))
+		l.readChar()
+		l.readChar()
+	} else if isLetter(l.ch) {
+		tok = token.New(l.handleIdentifier())
+	} else if isDigit(l.ch) {
+		tok = token.New(token.INT, l.readNumber())
+	} else if val, ok := singleCharMatch[l.ch]; ok {
+		tok = token.New(val, string(l.ch))
+		l.readChar()
+	} else {
+		tok = token.New(token.ILLEGAL, "")
+	}
+
+	return tok
+}
+
+func (l *Lexer) readNumber() string {
+	pos := l.position
+	for isDigit(l.ch) {
+		l.readChar()
+	}
+	return l.input[pos:l.position]
+}
+
+func (l *Lexer) readIdentifier() string {
+	pos := l.position
+	for isLetter(l.ch) {
+		l.readChar()
+	}
+	return l.input[pos:l.position]
+}
+
+func (l *Lexer) handleIdentifier() (token.TokenType, string) {
+	val := l.readIdentifier()
+	if match, ok := keywordMatch[val]; ok {
+		return match, val
+	} else {
+		return token.IDENT, val
+	}
+}
+
+func isLetter(r rune) bool {
+	return 'a' <= r && r <= 'z' || r == '_' || r == '?' || r == '!' // TODO: Deal with bang separately
+}
+
+func isDigit(ch rune) bool {
+	return '0' <= ch && ch <= '9'
+}
+
+func (l *Lexer) eatWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+func (l *Lexer) peekChar() rune {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		ch, _ := utf8.DecodeRuneInString(l.input[l.readPosition:])
+		return ch
+	}
+}
---
+++ token/token.go
@@ -0,0 +1,66 @@
+package token
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	ASSIGN    = "="
+	PLUS      = "+"
+	MINUS     = "-"
+	SLASH     = "/"
+	ASTERISK  = "*"
+	BANG      = "!"
+	CARET     = "^"
+	PIPE      = "|"
+	AMPERSAND = "&"
+	LANG      = "<"
+	RANG      = ">"
+	PERCENT   = "%"
+
+	EQ      = "=="
+	NEQ     = "!="
+	GEQ     = ">="
+	LEQ     = "<="
+	XOR_EQ  = "^="
+	OR_EQ   = "|="
+	AND_EQ  = "&="
+	PLUS_EQ = "+="
+	MIN_EQ  = "-="
+	MUL_EQ  = "*="
+	DIV_EQ  = "/="
+	PERC_EQ = "%="
+	AND     = "&&"
+	OR      = "||"
+	SHOVL   = "<<"
+	SHOVR   = ">>"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+	TRUE     = "TRUE"
+	FALSE    = "FALSE"
+	RETURN   = "RETURN"
+	IF       = "IF"
+	ELSE     = "ELSE"
+)
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+func New(t TokenType, v string) Token {
+	return Token{Type: t, Literal: v}
+}
