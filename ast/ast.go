fa415ca55bf45651e7ef7b626224fce13a03344b
Slashscreen <SlashScreen@users.noreply.github.com>
2024-01-15 19:06:42 -0800

    Added string method

--- ast/ast.go
+++ ast/ast.go
@@ -1,12 +1,13 @@
 package ast
 
 import (
-	"fmt"
+	"bytes"
 	"monkey/token"
 )
 
 type Node interface {
 	TokenLiteral() string
+	String() string
 }
 
 type Statement interface {
@@ -33,6 +34,16 @@ func (p *Program) TokenLiteral() string {
 	}
 }
 
+func (p *Program) String() string {
+	var out bytes.Buffer
+
+	for _, s := range p.Statements {
+		out.WriteString(s.String())
+	}
+
+	return out.String()
+}
+
 // LET STATEMENT
 
 type LetStatement struct {
@@ -43,6 +54,21 @@ type LetStatement struct {
 
 func (ls *LetStatement) statementNode()       {}
 func (ls *LetStatement) TokenLiteral() string { return ls.Token.Literal }
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
 
 // RETURN STATEMENT
 
@@ -53,6 +79,15 @@ type ReturnStatement struct {
 
 func (rs *ReturnStatement) statementNode()       {}
 func (rs *ReturnStatement) TokenLiteral() string { return rs.Token.Literal }
+func (rs *ReturnStatement) String() string {
+	var out bytes.Buffer
+	out.WriteString(rs.TokenLiteral() + " ")
+	if rs.ReturnValue != nil {
+		out.WriteString(rs.ReturnValue.String())
+	}
+	out.WriteString(";")
+	return out.String()
+}
 
 // IDENTIFIER
 
@@ -63,10 +98,11 @@ type Identifier struct {
 
 func (i *Identifier) expressionNode()      {}
 func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
 
 // OPERATOR
 
-type OperatorExpression struct {
+/* type OperatorExpression struct {
 	Left     Expression
 	Right    Expression
 	Operator token.TokenType
@@ -75,4 +111,20 @@ type OperatorExpression struct {
 func (o *OperatorExpression) expressionNode() {}
 func (o *OperatorExpression) TokenLiteral() string {
 	return fmt.Sprintf("%q %q %q", o.Left.TokenLiteral(), o.Operator, o.Right.TokenLiteral())
+} */
+
+// EXPRESSION STATEMENT
+
+type ExpressionStatement struct {
+	Token      token.Token
+	Expression Expression
+}
+
+func (es *ExpressionStatement) statementNode()       {}
+func (es *ExpressionStatement) TokenLiteral() string { return es.Token.Literal }
+func (es *ExpressionStatement) String() string {
+	if es.Expression != nil {
+		return es.Expression.String()
+	}
+	return ""
 }
